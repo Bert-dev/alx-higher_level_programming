e748a3ddca72966c54c6ce3fad13e9752259523a
Bert-Dev <adesinaelizabeth7@gmail.com>
2024-01-04 01:02:48 -0800

    python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, pbject-oriented programming\
+        language that combines remarkabke power with very clear syntax"
+str = str[39:66] + str[-23:-17] + str[:6]
+print(str)
